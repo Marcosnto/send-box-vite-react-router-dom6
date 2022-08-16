b1b906e98aed821271e59000b7d73d451e3a9c23
marcos <marcos_nto@hotmail.com>
2022-08-16 11:52:16 -0300

    organize files and remove default archives

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Home } from "./components/Home";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
-)
+);
