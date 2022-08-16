be918d829a3802c387d01c1c4fa60067d350ff9f
marcos <marcos_nto@hotmail.com>
2022-08-16 12:15:31 -0300

    create menu and use Link from react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from "./components/Home";
+import { About } from "./components/About";
 import "./styles/global.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Menu } from "./components/Menu";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Menu />
+    </BrowserRouter>
   </React.StrictMode>
 );
